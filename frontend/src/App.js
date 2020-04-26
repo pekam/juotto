2c1c610e9b34b699b1c529003353d5d4c233e2d0
Pekka Maanpää <pekkamaa@vaadin.com>
2020-04-26 12:35:36 +0300

    Add prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div>
-        Juotto
-    </div>
-  );
+  return <div>Juotto</div>;
 }
 
 export default App;
