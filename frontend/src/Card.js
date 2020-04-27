b33f8bf9e6f92de1ba917e9939ca995d782d3c46
Pekka Maanpää <pekkamaa@vaadin.com>
2020-04-27 20:43:52 +0300

    Reduce card size

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -2,5 +2,5 @@ import React from "react";
 
 export default ({ card }) => {
   const image = require(`./cards/${card.rank}${card.suite}.svg`);
-  return <img src={image} />;
+  return <img className="card" src={image} />;
 };
